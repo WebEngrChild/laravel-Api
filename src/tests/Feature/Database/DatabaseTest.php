f448e94d3451caab3da88ae43d4a58e78d26a519
joe-main <qmmplusfact@gmail.com>
2021-10-08 11:14:40 +0900

    Add the test code in /Featrue/Database/DatabaseTest.php

---
+++ src/tests/Feature/Database/DatabaseTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature\Database;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema; //追加
+use App\Department; //追加
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    //■■テスト１■■
+    //LaravelのDBに依存しない操作モデルであるSchemaを用いてカラムチェック
+    // public function testHasColumns()
+    // {
+    //     $this->assertTrue(
+    //         Schema::hasColumns('departments', [
+    //             'id', 'created_at', 'updated_at','name'
+    //         ]),
+    //         1
+    //     );
+    // }
+
+    //■■テスト２■■
+    //設定したテーブルにデータが挿入できるか確認
+    // public function testInsertData()
+    // {
+    //     $department = new Department();
+    //     $department->created_at = date("Y/m/d H:i:s");
+    //     $department->updated_at = date("Y/m/d H:i:s");
+    //     $department->name = 'department_name_test_1';
+    //     $saveDepartment = $department->save();
+
+    //     $this->assertTrue($saveDepartment);
+    // }
+
+    //■■テスト３■■
+    //レコード内容のチェック
+    // public function testCheckRecod()
+    // {
+    //     $department_name = [
+    //         'name' => 'department_name_test_1',
+    //     ];
+    //     $this->assertDatabaseHas('departments', $department_name);
+    // }
+
+    //■■テスト４■■
+    //テスト２＋３を同じ関数内で行う
+    use RefreshDatabase; //このトレイトでテスト利用後にデータを破棄してくれる
+
+    public function testCheckRecod()
+    {
+        //DBの登録
+        $department = new Department();
+        $department->created_at = date("Y/m/d H:i:s");
+        $department->updated_at = date("Y/m/d H:i:s");
+        $department->name = 'department_name_test_1';
+        $saveDepartment = $department->save();
+
+        //カラムの確認
+        $department_name = [
+            'name' => 'department_name_test_1',
+        ];
+        $this->assertDatabaseHas('departments', $department_name);
+    }
+
+    // ■■サンプル用■■
+    // /**
+    //  * A basic feature test example.
+    //  *
+    //  * @return void
+    //  */
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+}
--- src/tests/Unit/Sample/SampleTest.php
+++ src/tests/Unit/Sample/SampleTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit\Sample;
 
-// use PHPUnit\Framework\TestCase; //これだとヘルパー関数が使用できない↓↓にする！
+// use PHPUnit\Framework\TestCase; //これはFeature用のテストであるためヘルパー関数が使用できない↓↓にする！
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -12,7 +12,7 @@ use App\Http\Controllers\DebugController; //DebugControllerの呼び出し
 
 class SampleTest extends TestCase
 {
-    //■■テスト１：TestController
+    //■■テスト１：TestController■■
     // public function test_add()
     // {
     //     $test = new TestController;
@@ -30,7 +30,7 @@ class SampleTest extends TestCase
     //     $this->assertEquals(2, $sum);
     // }
 
-    //■■テスト２：DebugControllerの返却値='処理結果'を確認する
+    //■■テスト２：DebugControllerの返却値='処理結果'を確認する■■
     // public function test_statusCode()
     // {
     //     $debug = new DebugController;
@@ -38,10 +38,12 @@ class SampleTest extends TestCase
     //     $this->assertSame('処理結果', $response);
     // }
 
-    public function setUp():void
-    {
-        dd(env('APP_ENV'), env('DB_HOST'), env('DB_DATABASE'));
-        parent::setUp();
-    }
+    //■■Docker上にテスト用のMySQLコンテナを立ち上げる■■
+    //test用の.env.testingを使用できているかを確認する
+    // public function setUp():void
+    // {
+    //     dd(env('APP_ENV'), env('DB_HOST'), env('DB_DATABASE'));
+    //     parent::setUp();
+    // }
 
 }
